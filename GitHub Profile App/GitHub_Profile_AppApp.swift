a8211795d460a16404ff2f4d6d2443c25d1e8aef
Lalana Chamika <lalanachamika123@gmail.com>
2025-04-04 20:13:44 -0400

    Initializing the swiftui project

---
+++ GitHub Profile App/GitHub_Profile_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_Profile_AppApp.swift
+//  GitHub Profile App
+//
+//  Created by Lalana Thanthirigama on 2025-04-04.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_Profile_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
